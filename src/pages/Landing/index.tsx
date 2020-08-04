c63123282dd29e050e74f77bd9de0839235a1b51
Gabriel <silvagabrielbeltrame@gmail.com>
2020-08-04 08:52:30 -0300

    page TeacherList

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import logoImg from "../../assets/images/logo.svg";
 import landingImg from "../../assets/images/landing.svg";
 
@@ -7,7 +7,7 @@ import studyIcon from "../../assets/images/icons/study.svg";
 import giveClassesIcon from "../../assets/images/icons/give-classes.svg";
 import purpleHeartIcon from "../../assets/images/icons/purple-heart.svg";
 
-import './styles.css'
+import "./styles.css";
 
 function Landing() {
   return (
@@ -25,17 +25,18 @@ function Landing() {
         />
 
         <div className="buttons-container">
-            <a href="" className="study">
-                <img src={studyIcon} alt="Estudar"/> Estudar
-            </a>
+          <Link to="/study" className="study">
+            <img src={studyIcon} alt="Estudar" /> Estudar
+          </Link>
 
-            <a href="" className="give-classes">
-                <img src={giveClassesIcon} alt="Dar aulas"/> Dar aulas
-            </a>
+          <Link to="/give-classes" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas" /> Dar aulas
+          </Link>
         </div>
 
         <span className="total-connections">
-            Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
+          Total de 200 conexões já realizadas{" "}
+          <img src={purpleHeartIcon} alt="Coração roxo" />
         </span>
       </div>
     </div>
@@ -43,7 +44,5 @@ function Landing() {
 }
 
 export default Landing;
-
-
 
 // TODO: Parei na Aula 03/08 (Acelerando sua evolução) - 1:28:57
